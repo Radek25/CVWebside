71eb75e2688d3bdc508cfabf404ae96acdf8ef73
My Name <66422260+Radek25@users.noreply.github.com>
2021-08-03 14:04:10 +0200

    Create HomePage and rebuild structure of HTML and CSS code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const sections = document.querySelectorAll('section');
-const listOfDots = document.querySelectorAll('li');
+const listOfMenu = document.querySelectorAll('#menu-list > li')
+const listOfDots = document.querySelectorAll('#dots-list > li');
 const arrowUp = document.querySelector('#arrow-up');
 const arrowDown = document.querySelector('#arrow-down');
 arrowUp.innerHTML = '<i class="fas fa-chevron-up"></i>';
@@ -8,12 +9,15 @@ const polishLang = document.querySelector('#polish-lang');
 const englishLang = document.querySelector('#english-lang');
 let numberOfPage = 0;
 
-AddIdToScrollDotsAndDeflautStyles();
+AddIdAndDeflautStylesToNavs();
 SetLanguage();
 UseArrowsToScrollPage();
 UseDotsToScrollPage();
 
-function AddIdToScrollDotsAndDeflautStyles(){
+function AddIdAndDeflautStylesToNavs(){
+    listOfMenu.forEach((listOfMenu, index) => {
+        listOfMenu.id = index;
+    })
     listOfDots.forEach((listOfDots, index) => {
         listOfDots.id = index;
         listOfDots.style.setProperty('--backgroundColorOfDot', '#ffffff28');
@@ -22,27 +26,23 @@ function AddIdToScrollDotsAndDeflautStyles(){
 }
 function UseArrowsToScrollPage(){
     arrowDown.addEventListener('click', () => {
-        console.log('click');
-        console.log(numberOfPage);
         numberOfPage++;
-        if(numberOfPage <= 3){
+        if(numberOfPage <= 4){
             ScrollPage();
-            SetStandardScrollDots();
-            SetSpecialScrollDot(numberOfPage);
+            SetStandardStylesToMenuAndDotsNav();
+           SetSpecialStylesToMenuAndDotsNav(numberOfPage);
             arrowUp.style.display = 'flex';
         }
-        if(numberOfPage === 3){
+        if(numberOfPage === 4){
             arrowDown.style.display = 'none';
         }
     });
     arrowUp.addEventListener('click', () => {
-        console.log('click');
-        console.log(numberOfPage);
         numberOfPage--;
         if(numberOfPage >= 0){
             ScrollPage();
-            SetStandardScrollDots();
-            SetSpecialScrollDot(numberOfPage);
+            SetStandardStylesToMenuAndDotsNav();
+            SetSpecialStylesToMenuAndDotsNav(numberOfPage);
             arrowDown.style.display = 'flex';
         }
         if(numberOfPage === 0){
@@ -54,30 +54,30 @@ function UseDotsToScrollPage(){
     for (let index = 0; index < listOfDots.length; index++) {
         listOfDots[0].style.setProperty('--backgroundColorOfDot', '#ffffff');
         listOfDots[0].style.setProperty('--sizeOfDot', '15px');
+        listOfMenu[0].style.color = '#f4ac34';
+        listOfMenu[0].style.borderBottom = '1px solid #f4ac34';
         listOfDots[index].addEventListener('click', () => {
-            SetStandardScrollDots();
-            SetSpecialScrollDot(index);
+            SetStandardStylesToMenuAndDotsNav();
+            SetSpecialStylesToMenuAndDotsNav(index);
             sections.forEach((section, i) => {
                 if(i === index){
                 section.scrollIntoView({behavior: 'smooth'});
                 numberOfPage = index;
                 }
             })
-            switch (index) {
-                case (0):
-                    arrowUp.style.display = 'none';
-                    arrowDown.style.display = 'flex';
-                break;
-                case (3):
-                    arrowUp.style.display = 'flex';
-                    arrowDown.style.display = 'none';
-                break;
-                default:
-                    arrowUp.style.display = 'flex';
-                    arrowDown.style.display = 'flex';
-                break;
-            }
+            SetArrows(index);
         });
+        listOfMenu[index].addEventListener('click', () => {
+            console.log(index);
+            SetStandardStylesToMenuAndDotsNav();
+            SetSpecialStylesToMenuAndDotsNav(index);
+            sections.forEach((section, i) => {
+                if(i === index){
+                section.scrollIntoView({behavior: 'smooth'});
+                numberOfPage = index;
+                }
+            })
+        })
     }
 }
 function ScrollPage(){
@@ -87,15 +87,21 @@ function ScrollPage(){
         }
     })
 }
-function SetStandardScrollDots(){
+function SetStandardStylesToMenuAndDotsNav(){
     listOfDots.forEach(listOfDots => {
         listOfDots.style.setProperty('--backgroundColorOfDot', '#ffffff28');
         listOfDots.style.setProperty('--sizeOfDot', '10px');
     })
+    listOfMenu.forEach(listOfMenu => {
+        listOfMenu.style.color = '#ffffff'
+        listOfMenu.style.borderBottom = 'none';
+    })
 }
-function SetSpecialScrollDot(indexOfDot){
+function SetSpecialStylesToMenuAndDotsNav(indexOfDot){
     listOfDots[indexOfDot].style.setProperty('--backgroundColorOfDot', '#ffffff');
     listOfDots[indexOfDot].style.setProperty('--sizeOfDot', '15px');
+    listOfMenu[indexOfDot].style.color = '#f4ac34';
+    listOfMenu[indexOfDot].style.borderBottom = '1px solid #f4ac34';
 }
 function SetLanguage(){
     polishLang.addEventListener('click', () => {
@@ -110,4 +116,20 @@ function SetLanguage(){
         polishLang.style.borderRight= 'none';
         englishLang.style.borderLeft = '2px solid white';
     });
+}
+function SetArrows(indexOfPage){
+    switch (indexOfPage) {
+        case (0):
+            arrowUp.style.display = 'none';
+            arrowDown.style.display = 'flex';
+        break;
+        case (4):
+            arrowUp.style.display = 'flex';
+            arrowDown.style.display = 'none';
+        break;
+        default:
+            arrowUp.style.display = 'flex';
+            arrowDown.style.display = 'flex';
+        break;
+    }
 }
